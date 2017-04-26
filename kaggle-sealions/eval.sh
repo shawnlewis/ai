06680e0e2a10a8dbee5053aba31a0306f91c57d2
Shawn Lewis <shlewis@gmail.com>
2017-04-25 18:05:55 -0700

    Add eval scripts.

---
+++ kaggle-sealions/eval.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PYTHONPATH=~/tp_code/models/slim
+export PYTHONPATH
+
+DATASET_DIR=~/datasets/kaggle-sealions/tensorflow
+TRAIN_DIR=~/train/kaggle-sealions/current
+if [ -n "$1" ]; then
+    checkpoint_path="$1"
+else
+    checkpoint_path="$TRAIN_DIR"
+fi
+python eval_image_classifier.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=sealions \
+    --dataset_split_name=validation \
+    --model_name=inception_resnet_v2 \
+    --checkpoint_path=${checkpoint_path}
